3a01e06207845b37fd82ef651f203cd4b726d1f5
Teja Madu <tejamadu@Mac.lan>
2026-04-03 17:32:07 -0500

    running both apps together

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,6 @@
-import { serve } from "bun";
-import index from "./index.html";
+import concurrently from 'concurrently';
 
-const server = serve({
-  routes: {
-    // Serve index.html for all unmatched routes.
-    "/*": index,
-
-    "/api/hello": {
-      async GET(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "GET",
-        });
-      },
-      async PUT(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "PUT",
-        });
-      },
-    },
-
-    "/api/hello/:name": async req => {
-      const name = req.params.name;
-      return Response.json({
-        message: `Hello, ${name}!`,
-      });
-    },
-  },
-
-  development: process.env.NODE_ENV !== "production" && {
-    // Enable browser hot reloading in development
-    hmr: true,
-
-    // Echo console logs from the browser to the server
-    console: true,
-  },
-});
-
-console.log(`🚀 Server running at ${server.url}`);
+concurrently([
+    { command: 'bun dev', name: 'server', cwd: 'packages/server', prefixColor: 'blue' },
+    { command: 'bun dev', name: 'client', cwd: 'packages/client', prefixColor: 'red' },
+]);
