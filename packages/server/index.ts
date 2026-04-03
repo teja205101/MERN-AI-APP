17cb318cb2b823773cf50faf81449c1ac87015de
Teja Madu <tejamadu@Mac.lan>
2026-04-02 19:31:38 -0500

    Initial commit

---
+++ packages/server/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+import type { Request, Response } from "express";
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`Api Key : ${process.env.API_KEY}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
