c46eca705e34fb098ed611c9d33440a7ed76b054
Teja Madu <tejamadu@Mac.lan>
2026-04-06 17:39:28 -0500

    adding shadcn-ui

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,11 +1,18 @@
-import './index.css'
-function App() {
+import "./index.css";
+// import { Button } from "@/components/ui/button";
+import { CircleFadingArrowUpIcon } from "lucide-react";
 
+function App() {
   return (
-    <>
-      <p className="text-2xl font-bold text-red-500 p-6">Hello Teja Congrats on new Ai Full Stack MERN AI Project Happy for you :)</p>
-    </>
-  )
+    <div className="p-6">
+      <p className="text-2xl font-bold text-red-500 p-6">
+        Hello Teja Congrats on new Ai Full Stack MERN AI Project Happy for you
+        :)
+      </p>
+      {/* <Button variant="outline">Button</Button> */}
+      <CircleFadingArrowUpIcon />
+    </div>
+  );
 }
 
-export default App
+export default App;
