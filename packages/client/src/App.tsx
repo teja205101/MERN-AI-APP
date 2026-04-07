731015771d14ec405d4a41a8af6ecdf82f9833f7
Teja Madu <tejamadu@Mac.lan>
2026-04-07 17:57:38 -0500

    setup husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,5 @@
 import './index.css';
-// import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button"
 import { CircleFadingArrowUpIcon } from 'lucide-react';
 
 function App() {
