918a1179b4304491abe8c942f18a5763361674da
Teja Madu <tejamadu@Mac.lan>
2026-04-21 15:21:36 -0500

    Abstarcting services.

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,19 +1,14 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import OpenAI from 'openai';
 import z from 'zod';
-import { connverstaionRepository } from './repositories/conversation.repository';
+import { chatService } from './services/chat.service';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
-
-const client = new OpenAI({
-   apiKey: process.env.API_KEY,
-});
 
 app.get('/', (req: Request, res: Response) => {
    //   res.send(`Api Key : ${process.env.API_KEY}`);
@@ -43,17 +38,9 @@ app.post('/api/chat', async (req: Request, res: Response) => {
    try {
       const { prompt, conversationId } = req.body;
 
-      const response = await client.responses.create({
-         model: 'gpt-4o-mini',
-         input: prompt,
-         temperature: 0.3,
-         max_output_tokens: 100,
-         previous_response_id:
-            connverstaionRepository.getLastResponseId(conversationId),
-      });
+      const response = await chatService.sendMessage(prompt, conversationId);
 
-      connverstaionRepository.setLastResponseId(conversationId, response.id);
-      res.json({ message: response.output_text });
+      res.json({ message: response.message });
    } catch (error) {
       res.status(500).json({
          error: `Failed to generate a response bcz of ${error}`,
---
+++ packages/server/services/chat.service.ts
@@ -0,0 +1,33 @@
+import OpenAI from 'openai';
+import { connverstaionRepository } from '../repositories/conversation.repository';
+
+interface ChatResponse {
+   Id: string;
+   message: string;
+}
+// Implementation detail
+const client = new OpenAI({
+   apiKey: process.env.API_KEY,
+});
+
+// Public Interface
+// Leaky abstraction
+export const chatService = {
+   async sendMessage(
+      prompt: string,
+      conversationId: string
+   ): Promise<ChatResponse> {
+      const response = await client.responses.create({
+         model: 'gpt-4o-mini',
+         input: prompt,
+         temperature: 0.3,
+         max_output_tokens: 100,
+         previous_response_id:
+            connverstaionRepository.getLastResponseId(conversationId),
+      });
+
+      connverstaionRepository.setLastResponseId(conversationId, response.id);
+
+      return { id: response.id, message: response.output_text };
+   },
+};
